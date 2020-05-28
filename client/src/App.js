d21ecf9d13076a339f8c051bb97f981af66c11b1
daesik.park <daesik.park@ahha.tf>
2020-05-28 18:03:41 +0900

    Add Node.js Express Server Module

---
+++ client/src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import Customer from './components/Customer'
+import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3, 
+    overflowX: 'auto'
+  },
+  table:{
+    minWidth: 1080
+  }
+})
+
+const customers = [{
+  'id':1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '차인표',
+  'birthday': '94132',
+  'gender' :'남자',
+  'job':'배우'
+},
+{
+  'id':2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '송종국',
+  'birthday': '75231',
+  'gender' :'남자',
+  'job':'축구선수'
+},
+{
+  'id':3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '박지성',
+  'birthday': '950111',  
+  'gender' :'남자',
+  'job':'축구선수'
+},
+{
+  'id':4,
+  'image': 'https://placeimg.com/64/64/4',
+  'name': '임동선',
+  'birthday': '94132',
+  'gender' :'남자',
+  'job':'대학생'
+}
+]
+
+class App extends Component {
+  render() {
+  const { classes } = this.props;
+  return (
+      <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell> 번호 </TableCell>
+            <TableCell> 이미지 </TableCell>
+            <TableCell> 이름 </TableCell>
+            <TableCell> 생년월일 </TableCell>
+            <TableCell> 성별 </TableCell>
+            <TableCell> 직업 </TableCell>
+
+          </TableRow>
+        </TableHead>
+       <TableBody>
+      { customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}  />
+          )})}
+           </TableBody>
+         </Table>
+      </Paper>
+   );
+  }
+}
+
+export default withStyles(styles)(App);
